936ae8aa0989563ec462d3386a0f432e60aa391c
Marco Rinaldi <marco.rinaldi9@live.com>
2019-07-28 21:22:07 +0200

    Created dependancy resolver for the Dependancy Injection container

--- SportsStore.WebUI/App_Start/Ninject.Web.Common.cs
+++ SportsStore.WebUI/App_Start/Ninject.Web.Common.cs
@@ -5,26 +5,31 @@ namespace SportsStore.WebUI.App_Start
 {
     using System;
     using System.Web;
+    using System.Web.Mvc;
 
     using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 
     using Ninject;
     using Ninject.Web.Common;
+    using Ninject.Web.Common.WebHost;
 
-    public static class NinjectWebCommon 
+    using SportsStore.WebUI.Infrastructure;
+
+
+    public static class NinjectWebCommon
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
         /// Starts the application
         /// </summary>
-        public static void Start() 
+        public static void Start()
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
             bootstrapper.Initialize(CreateKernel);
         }
-        
+
         /// <summary>
         /// Stops the application.
         /// </summary>
@@ -32,7 +37,7 @@ namespace SportsStore.WebUI.App_Start
         {
             bootstrapper.ShutDown();
         }
-        
+
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
@@ -60,6 +65,7 @@ namespace SportsStore.WebUI.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-        }        
+            DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
+        }
     }
 }
