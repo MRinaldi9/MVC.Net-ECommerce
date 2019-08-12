d436f634a669b16fa20a0dbb3c281e10011daf55
Marco Rinaldi <marco.rinaldi9@live.com>
2019-08-12 19:09:44 +0200

    Added the Domain Model, set a "fake" repository pattern for debugging and created a mock repository

---
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Abstract
+{
+    //Define and Initialization for repository pattern
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Moq;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -20,6 +23,18 @@ namespace SportsStore.WebUI.Infrastructure
         private void AddBindings()
         {
             //Put bindings here
+            
+            //Fake repos
+            Mock<IProductRepository> mockRepository = new Mock<IProductRepository>();
+            mockRepository.Setup(repos => repos.Products).Returns(new List<Product>
+            {
+                new Product {Name = "Football",Price = 25},
+                new Product {Name = "Surf Board",Price = 179},
+                new Product {Name = "Running shoes",Price = 95},
+            });
+
+            //Every time i call an object that implement IProductRepository i obtain the same mock object
+            kernel.Bind<IProductRepository>().ToConstant(mockRepository.Object);
         }
 
         public object GetService(Type serviceType)
