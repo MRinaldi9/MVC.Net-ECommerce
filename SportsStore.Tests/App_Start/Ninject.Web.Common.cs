ce00281e75c9948d89bb2915e645c5c340a79223
Marco Rinaldi <marco.rinaldi9@live.com>
2019-08-14 16:50:12 +0200

    Started unit testing and changed return type for the action List

--- SportsStore.Tests/App_Start/Ninject.Web.Common.cs
+++ SportsStore.Tests/App_Start/Ninject.Web.Common.cs
@@ -10,6 +10,7 @@ namespace SportsStore.Tests.App_Start
 
     using Ninject;
     using Ninject.Web.Common;
+    using Ninject.Web.Common.WebHost;
 
     public static class NinjectWebCommon 
     {
--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -11,15 +11,19 @@ namespace SportsStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
-        
+        public int PageSize = 4;
+
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
 
-        public ActionResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(repository.Products
+                        .OrderBy(product => product.ProductID)
+                        .Skip((page - 1) * PageSize)
+                        .Take(PageSize));
         }
     }
 }
