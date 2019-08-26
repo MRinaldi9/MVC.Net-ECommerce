713b7c9d55d42de0ece810cae048ce7ea9555859
Marco Rinaldi <marco.rinaldi9@live.com>
2019-08-26 09:43:32 +0200

    Added tests for admin page

--- SportsStore.Tests/AdminTests.cs
+++ SportsStore.Tests/AdminTests.cs
@@ -33,5 +33,45 @@ namespace SportsStore.Tests
             Assert.AreEqual("P2", result[1].Name);
             Assert.AreEqual("P3", result[2].Name);
         }
+
+        [TestMethod]
+        public void Can_Edit_Product()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product{ProductID = 1,Name = "P1"},
+                new Product{ProductID = 2,Name = "P2"},
+                new Product{ProductID = 3,Name = "P3"}
+            });
+
+            AdminController controller = new AdminController(mock.Object);
+
+            Product p1 = controller.Edit(1).ViewData.Model as Product;
+            Product p2 = controller.Edit(2).ViewData.Model as Product;
+            Product p3 = controller.Edit(3).ViewData.Model as Product;
+
+            Assert.AreEqual(1, p1.ProductID);
+            Assert.AreEqual(2, p2.ProductID);
+            Assert.AreEqual(3, p3.ProductID);
+        }
+
+        [TestMethod]
+        public void Canno_Edit_Nonexisten_Product()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+             {
+                new Product{ProductID = 1,Name = "P1"},
+                new Product{ProductID = 2,Name = "P2"},
+                new Product{ProductID = 3,Name = "P3"}
+             });
+
+            AdminController controller = new AdminController(mock.Object);
+
+            Product p4 = controller.Edit(4).ViewData.Model as Product;
+
+            Assert.IsNull(p4);
+        }
     }
 }
